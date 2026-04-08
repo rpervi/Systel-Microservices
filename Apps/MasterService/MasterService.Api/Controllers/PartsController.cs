915b33502d00410def7bf074e607fe58187fed0b
rpervi <rpervi@inevia.co>
2026-04-08 11:17:06 +0530

    no changes 3

--- Apps/MasterService/MasterService.Api/Controllers/PartsController.cs
+++ Apps/MasterService/MasterService.Api/Controllers/PartsController.cs
@@ -24,6 +24,9 @@ public class PartsController : ControllerBase
         return Ok(parts);
     }
 
+
+
+
     // GET: api/parts/5
     [HttpGet("{id}")]
     public async Task<ActionResult<PartMaster>> GetPart(int id)
